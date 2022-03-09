f17372be97ef80fe89991e0769d286cda380c59c
Aadharv <101254088+Aadharv@users.noreply.github.com>
2022-03-09 15:49:15 +0530

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,19 @@
+print("Simple Calculator")
+
+x=7
+y=8
+
+sum=x+y
+
+difference=x-y
+
+product=x*y
+
+division=x/y
+remainder=x%y
+
+print("The sum of X and Y is",sum)
+print("The difference between X and Y is",difference)
+print("The product of X and Y is",product)
+print("The uotient of X and Y is",division)
+print("The remainder of X and Y is",remainder)
